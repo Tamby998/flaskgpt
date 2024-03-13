c0a409e19f3aa0ee74dd2b75dd08805d082ed9a0
Tamby Mampionona <theriharson@email.com>
2024-03-13 09:10:15 +0300

    Ajout du clé d'OPENAI

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+import os
+import openai
+from dotenv import load_dotenv
 from flask import Flask, render_template
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
